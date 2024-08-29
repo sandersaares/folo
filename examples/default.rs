c618b491e88b89a344e32952d87b5718cdc91343
Sander Saares <sander@saares.eu>
2024-08-29 18:48:15 +0300

    Accept any Output type

--- examples/default.rs
+++ examples/default.rs
@@ -31,6 +31,9 @@ fn main() {
     engine.enqueue(yield_now());
     engine.enqueue(yield_now());
 
+    // TODO: The above is scheduling work that does not expose its return value. What mechanisms
+    // do we need to expose the return value once ready?
+
     engine.run();
 }
 
@@ -38,7 +41,7 @@ fn main() {
 /// threads that are part of the same executor.
 struct Engine {
     // The active set contains all the futures we want to poll. This is where all futures start.
-    active: VecDeque<MyTask>,
+    active: VecDeque<Box<dyn Task>>,
 
     // The inactive set contains all the futures that are sleeping. We will move them back to the
     // active set once a waker notifies us that a future needs to wake up. Note that the wakeup
@@ -46,7 +49,7 @@ struct Engine {
     // of the inactive set immediately before polling it, and be ready to move it back to the active
     // set during the poll if a waker is signaled during a poll. Also implies executor is not locked
     // during a poll, so new activity can occur (not only wakes but also new tasks being added).
-    inactive: VecDeque<MyTask>,
+    inactive: VecDeque<Box<dyn Task>>,
 
     // The awakened set contains all the tasks that have been woken up and need to be moved back to
     // the active set. Note that our wakers may be called from any thread, so this must be guarded.
@@ -68,13 +71,12 @@ impl Engine {
         }
     }
 
-    // TODO: All this registration stuff is pretty gross. Can we clean it up somehow?
     fn enqueue(&mut self, future: impl Future<Output = ()> + 'static) {
-        self.active.push_back(MyTask::new(
+        self.active.push_back(Box::new(MyTask::new(
             self.next_task_id,
             Box::pin(future),
             Arc::new(MyWaker::new(self.next_task_id, Arc::clone(&self.awakened))),
-        ));
+        )));
 
         self.next_task_id += 1;
     }
@@ -87,11 +89,7 @@ impl Engine {
             }
 
             while let Some(mut task) = self.active.pop_front() {
-                // TODO: This allocation is gross. To be fixed later (requires unsafe code).
-                let waker = task.waker.clone().into();
-                let mut context = task::Context::from_waker(&waker);
-
-                match task.future.as_mut().poll(&mut context) {
+                match task.poll() {
                     task::Poll::Ready(()) => {
                         println!("Future completed.");
                     }
@@ -111,7 +109,7 @@ impl Engine {
                     println!("Found awakened task {}.", task_id);
                     awakened.remove(&task_id);
 
-                    let Some(index) = self.inactive.iter().position(|x| x.id == task_id) else {
+                    let Some(index) = self.inactive.iter().position(|x| x.id() == task_id) else {
                         // There is no limit on the lifetime of a waker, so one may trigger wakeups
                         // of tasks that no longer exist. Just ignore that if it happens.
                         println!("Task {} not found in inactive set.", task_id);
@@ -128,15 +126,44 @@ impl Engine {
     }
 }
 
-struct MyTask {
+trait Task {
+    fn id(&self) -> usize;
+    fn poll(&mut self) -> task::Poll<()>;
+}
+
+struct MyTask<R> {
     id: usize,
-    future: Pin<Box<dyn Future<Output = ()>>>,
+    future: Pin<Box<dyn Future<Output = R>>>,
     waker: Arc<MyWaker>,
 }
 
-impl MyTask {
-    fn new(id: usize, future: Pin<Box<dyn Future<Output = ()>>>, waker: Arc<MyWaker>) -> Self {
+impl<R> MyTask<R> {
+    fn new(id: usize, future: Pin<Box<dyn Future<Output = R>>>, waker: Arc<MyWaker>) -> Self {
         Self { id, future, waker }
+    }
+}
+
+impl<R> Task for MyTask<R> {
+    fn id(&self) -> usize {
+        self.id
+    }
+
+    fn poll(&mut self) -> task::Poll<()> {
+        // TODO: This allocation is gross. To be fixed later (requires unsafe code).
+        let waker = self.waker.clone().into();
+        let mut context = task::Context::from_waker(&waker);
+
+        // TODO: Deliver the output value to the join handle if we have finished.
+        match self.future.as_mut().poll(&mut context) {
+            task::Poll::Ready(_) => {
+                println!("Future completed.");
+                task::Poll::Ready(())
+            }
+            task::Poll::Pending => {
+                println!("Future returned Pending. Will poll again after wakeup.");
+                task::Poll::Pending
+            }
+        }
     }
 }
 
