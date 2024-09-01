a5fe4a3b1704a5cc378bcfd926f6af2665a3a671
Sander Saares <sander@saares.eu>
2024-09-01 11:49:56 +0300

    Add more benchmarks

--- crates/folo/benches/spawning.rs
+++ crates/folo/benches/spawning.rs
@@ -1,20 +1,23 @@
 use criterion::{async_executor::AsyncExecutor, criterion_group, criterion_main, Criterion};
+use folo::RemoteJoinHandle;
 
 criterion_group!(benches, spawn_and_await_many);
 criterion_main!(benches);
 
+const MANY_TASK_COUNT: usize = 1000;
+
 fn spawn_and_await_many(c: &mut Criterion) {
     let tokio = tokio::runtime::Builder::new_multi_thread().build().unwrap();
+    let tokio_local = tokio::runtime::Builder::new_current_thread().build().unwrap();
 
     let mut group = c.benchmark_group("spawn_and_await_many");
 
     group.bench_function("folo_remote", |b| {
         b.to_async(FoloExecutor).iter(|| {
             folo::spawn_on_any(async {
-                const TASK_COUNT: usize = 100_000;
-                let mut tasks = Vec::with_capacity(TASK_COUNT);
+                let mut tasks = Vec::with_capacity(MANY_TASK_COUNT);
 
-                for _ in 0..TASK_COUNT {
+                for _ in 0..MANY_TASK_COUNT {
                     tasks.push(folo::spawn_on_any(async { folo::yield_now().await }));
                 }
 
@@ -28,10 +31,9 @@ fn spawn_and_await_many(c: &mut Criterion) {
     group.bench_function("tokio_remote", |b| {
         b.to_async(&tokio).iter(|| {
             tokio::task::spawn(async {
-                const TASK_COUNT: usize = 100_000;
-                let mut tasks = Vec::with_capacity(TASK_COUNT);
+                let mut tasks = Vec::with_capacity(MANY_TASK_COUNT);
 
-                for _ in 0..TASK_COUNT {
+                for _ in 0..MANY_TASK_COUNT {
                     tasks.push(tokio::task::spawn(async { tokio::task::yield_now().await }));
                 }
 
@@ -42,22 +44,58 @@ fn spawn_and_await_many(c: &mut Criterion) {
         });
     });
 
-    // group.bench_function("folo_local", |b| {
-    //     b.to_async(FoloExecutor).iter(|| {
-    //         folo::spawn_on_any(async {
-    //             const TASK_COUNT: usize = 100_000;
-    //             let mut tasks = Vec::<RemoteJoinHandle<_>>::with_capacity(TASK_COUNT);
+    group.bench_function("tokio_local", |b| {
+        b.to_async(&tokio_local).iter(|| {
+            tokio::task::spawn(async {
+                let mut tasks = Vec::with_capacity(MANY_TASK_COUNT);
 
-    //             for _ in 0..TASK_COUNT {
-    //                 tasks.push(folo::spawn(async { folo::yield_now().await }).into());
-    //             }
+                for _ in 0..MANY_TASK_COUNT {
+                    tasks.push(tokio::task::spawn(async { tokio::task::yield_now().await }));
+                }
 
-    //             for task in tasks {
-    //                 _ = task.await;
-    //             }
-    //         })
-    //     });
-    // });
+                for task in tasks {
+                    _ = task.await;
+                }
+            })
+        });
+    });
+
+    group.bench_function("folo_local_as_remote", |b| {
+        b.to_async(FoloExecutor).iter(|| {
+            folo::spawn_on_any(async {
+                let mut tasks = Vec::<RemoteJoinHandle<_>>::with_capacity(MANY_TASK_COUNT);
+
+                for _ in 0..MANY_TASK_COUNT {
+                    tasks.push(folo::spawn(async { folo::yield_now().await }).into());
+                }
+
+                for task in tasks {
+                    _ = task.await;
+                }
+            })
+        });
+    });
+
+    group.bench_function("folo_local", |b| {
+        b.to_async(FoloExecutor).iter(|| {
+            folo::spawn_on_any(async {
+                let root_handle: RemoteJoinHandle<_> = folo::spawn(async {
+                    let mut tasks = Vec::with_capacity(MANY_TASK_COUNT);
+
+                    for _ in 0..MANY_TASK_COUNT {
+                        tasks.push(folo::spawn(async { folo::yield_now().await }));
+                    }
+
+                    for task in tasks {
+                        _ = task.await;
+                    }
+                })
+                .into();
+
+                root_handle.await;
+            })
+        });
+    });
 
     group.finish();
 }
