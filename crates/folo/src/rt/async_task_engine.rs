1f6414d53a99f8f5b857fc2129bcdb5e2b95a280
Sander Saares <sander@saares.eu>
2024-09-03 22:05:40 +0300

    Simplify awakening logic by using a flag instead of a set

--- crates/folo/src/rt/async_task_engine.rs
+++ crates/folo/src/rt/async_task_engine.rs
@@ -1,9 +1,13 @@
 use crate::rt::LocalErasedTask;
 use negative_impl::negative_impl;
 use std::{
-    collections::{BTreeSet, VecDeque},
+    collections::VecDeque,
     fmt::{self, Debug, Formatter},
-    sync::{Arc, Mutex},
+    pin::Pin,
+    sync::{
+        atomic::{AtomicBool, Ordering},
+        Arc,
+    },
     task::{self, Wake},
 };
 
@@ -21,16 +25,6 @@ pub struct AsyncTaskEngine {
     // set during the poll if a waker is signaled during a poll. Also implies engine is not locked
     // during a poll, so new activity can occur (not only wakes but also new tasks being added).
     inactive: VecDeque<Task>,
-
-    // The awakened set contains all the tasks that have been woken up and need to be moved back to
-    // the active set. Note that our wakers may be called from any thread, so this must be guarded.
-    //
-    // TODO: Ewww, a set implies allocations from foreign thread are permissible (because wakers
-    // are thread-safe, so we may be woken up by who knows what). We need an alloc-free mechanism.
-    // We probably need to make our tasks Arc instead of Box, so awakening becomes updating a flag.
-    awakened: Arc<Mutex<BTreeSet<usize>>>,
-
-    next_task_id: usize,
 }
 
 impl AsyncTaskEngine {
@@ -38,8 +32,6 @@ impl AsyncTaskEngine {
         Self {
             active: VecDeque::new(),
             inactive: VecDeque::new(),
-            awakened: Arc::new(Mutex::new(BTreeSet::new())),
-            next_task_id: 0,
         }
     }
 
@@ -47,17 +39,7 @@ impl AsyncTaskEngine {
     /// will be made available by the async task engine - it is expected that some other mechanism
     /// is used to observe the result.
     pub fn enqueue_erased(&mut self, erased_task: LocalErasedTask) {
-        let task = Task::new(
-            self.next_task_id,
-            erased_task,
-            Arc::new(TaskWaker::new(
-                self.next_task_id,
-                Arc::clone(&self.awakened),
-            )),
-        );
-
-        self.next_task_id += 1;
-
+        let task = Task::new(erased_task);
         self.active.push_back(task);
     }
 
@@ -77,22 +59,8 @@ impl AsyncTaskEngine {
             }
         }
 
-        let mut awakened = self.awakened.lock().unwrap();
-
-        while let Some(task_id) = awakened.pop_first() {
+        if self.activate_awakened_tasks() {
             had_activity = true;
-
-            awakened.remove(&task_id);
-
-            let Some(index) = self.inactive.iter().position(|x| x.id == task_id) else {
-                // There is no limit on the lifetime of a waker, so one may trigger wakeups
-                // of tasks that no longer exist. Just ignore that if it happens.
-                println!("Task {} not found in inactive set.", task_id);
-                continue;
-            };
-
-            let task = self.inactive.remove(index).unwrap();
-            self.active.push_back(task);
         }
 
         if had_activity {
@@ -100,6 +68,28 @@ impl AsyncTaskEngine {
         } else {
             CycleResult::Suspend
         }
+    }
+
+    // Moves any awakened tasks into the active set. Returns whether any tasks were moved.
+    fn activate_awakened_tasks(&mut self) -> bool {
+        let mut had_activity = false;
+
+        let mut index = 0;
+
+        while index < self.inactive.len() {
+            let task = &self.inactive[index];
+
+            if task.waker.consume_awakened() {
+                had_activity = true;
+
+                let task = self.inactive.remove(index).unwrap();
+                self.active.push_back(task);
+            } else {
+                index += 1;
+            }
+        }
+
+        had_activity
     }
 }
 
@@ -126,23 +116,21 @@ pub enum CycleResult {
 }
 
 struct Task {
-    id: usize,
     erased_task: LocalErasedTask,
     waker: Arc<TaskWaker>,
 }
 
 impl Task {
-    fn new(id: usize, erased_task: LocalErasedTask, waker: Arc<TaskWaker>) -> Self {
+    fn new(erased_task: LocalErasedTask) -> Self {
         Self {
-            id,
             erased_task,
-            waker,
+            waker: Arc::new(TaskWaker::new()),
         }
     }
 
     fn poll(&mut self) -> task::Poll<()> {
         // TODO: This allocation is gross. To be fixed later (requires unsafe code).
-        let waker = self.waker.clone().into();
+        let waker = Arc::clone(&self.waker).into();
         let mut context = task::Context::from_waker(&waker);
 
         self.erased_task.as_mut().poll(&mut context)
@@ -151,34 +139,43 @@ impl Task {
 
 impl Debug for Task {
     fn fmt(&self, f: &mut Formatter<'_>) -> fmt::Result {
-        f.debug_struct("Task")
-            .field("id", &self.id)
-            .field("waker", &self.waker)
-            .finish()
+        f.debug_struct("Task").field("waker", &self.waker).finish()
     }
 }
 
+#[repr(transparent)]
 struct TaskWaker {
-    task_id: usize,
-    awakened: Arc<Mutex<BTreeSet<usize>>>,
+    awakened: Pin<Arc<AtomicBool>>,
 }
 
 impl Wake for TaskWaker {
     fn wake(self: Arc<Self>) {
-        self.awakened.lock().unwrap().insert(self.task_id);
+        self.awakened.store(true, Ordering::Relaxed)
     }
 }
 
 impl TaskWaker {
-    fn new(task_id: usize, awakened: Arc<Mutex<BTreeSet<usize>>>) -> Self {
-        Self { task_id, awakened }
+    fn new() -> Self {
+        Self {
+            awakened: Pin::new(Arc::new(AtomicBool::new(false))),
+        }
+    }
+
+    /// Checks whether the waker has woken up. If so, resets the waker.
+    fn consume_awakened(&self) -> bool {
+        let is_awakened = self.awakened.load(Ordering::Relaxed);
+
+        if is_awakened {
+            self.awakened.store(false, Ordering::Relaxed);
+        }
+
+        is_awakened
     }
 }
 
 impl Debug for TaskWaker {
     fn fmt(&self, f: &mut Formatter<'_>) -> fmt::Result {
         f.debug_struct("TaskWaker")
-            .field("task_id", &self.task_id)
             .field("awakened", &self.awakened)
             .finish()
     }
