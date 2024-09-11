c210dccbaa34104d6035d13b0b8601249eca6a5a
Sander Saares <sander@saares.eu>
2024-09-11 23:43:53 +0300

    max_processors in tasks.rs

--- crates/folo/examples/tasks.rs
+++ crates/folo/examples/tasks.rs
@@ -4,7 +4,7 @@ use std::error::Error;
 const BATCH_SIZE: usize = 1000;
 const SUB_BATCH_SIZE: usize = 100;
 
-#[folo::main(print_metrics)]
+#[folo::main(print_metrics, max_processors = 1)]
 async fn main() -> Result<(), Box<dyn Error + Send + Sync + 'static>> {
     for _ in 0..10000 {
         let tasks = (0..BATCH_SIZE)
