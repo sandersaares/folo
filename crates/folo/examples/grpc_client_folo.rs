c0ebb0817fb584f2576e06416ec7dfddafbdbded
Martin Tomka <martintomka@microsoft.com>
2024-09-20 14:06:16 +0200

    Improve the example

--- crates/folo/examples/grpc_client_folo.rs
+++ crates/folo/examples/grpc_client_folo.rs
@@ -1,22 +1,42 @@
 //! This example runs a server that responds to any request with "Hello, world!"
 
-use std::{error::Error, net::SocketAddr};
+use std::{
+    error::Error,
+    net::ToSocketAddrs,
+};
 
-use folo::net::TcpStream;
+use folo::{
+    net::TcpStream,
+    rt::{spawn_sync, SynchronousTaskType},
+    time::{Clock, Stopwatch},
+};
 
 #[folo::main]
 async fn main() -> Result<(), Box<dyn Error + Send + Sync + 'static>> {
-    let ip: SocketAddr = "127.0.0.1:1234".parse().unwrap();
+    let clock = Clock::new();
 
-    match TcpStream::connect(ip).await {
-        Ok(stream) => {
-            println!("Connected: {}", stream.peer_addr());
+    // This can be blocking call, run it in a synchronous task
+    let sockets = spawn_sync(SynchronousTaskType::Syscall, || {
+        "microsoft.com:443".to_socket_addrs()
+    })
+    .await?;
+
+    for socket in sockets {
+        let stopwatch = Stopwatch::with_clock(&clock);
+
+        match TcpStream::connect(socket).await {
+            Ok(stream) => {
+                println!(
+                    "Connected: {}, Took: {}ms",
+                    stream.peer_addr(),
+                    stopwatch.elapsed().as_millis()
+                );
+            }
+            Err(e) => {
+                eprintln!("Failed to connect: {}", e);
+            }
         }
-        Err(e) => {
-            eprintln!("Failed to connect: {}", e);
-        }
-    } 
+    }
 
     Ok(())
 }
-
