0c9823256cbb770301181429cb6be29a26e09a91
Sander Saares <sander@saares.eu>
2024-09-20 08:33:32 +0300

    TCP shutdown as typed future

--- crates/folo/src/net/tcp_connection.rs
+++ crates/folo/src/net/tcp_connection.rs
@@ -1,11 +1,12 @@
 use crate::{
     io::{self, OperationResultExt, OperationResultFuture, PinnedBuffer},
     net::winsock,
-    rt::{current_async_agent, current_runtime, SynchronousTaskType},
+    rt::{current_async_agent, current_runtime, RemoteJoinHandle, SynchronousTaskType},
     windows::OwnedHandle,
 };
 use negative_impl::negative_impl;
-use std::sync::Arc;
+use pin_project::pin_project;
+use std::{future::Future, sync::Arc, task::Poll};
 use windows::{
     core::PSTR,
     Win32::Networking::WinSock::{WSARecv, WSASend, WSASendDisconnect, SOCKET, WSABUF},
@@ -27,29 +28,7 @@ impl TcpConnection {
     /// You should not call this multiple times concurrently because there is no guarantee that the
     /// continuations will be called in a particular order.
     pub fn receive(&mut self, buffer: PinnedBuffer) -> OperationResultFuture {
-        // SAFETY: We are required to pass the OVERLAPPED pointer to the completion routine. We do.
-        unsafe {
-            current_async_agent::with_io(|io| io.new_operation(buffer)).begin(
-                |buffer, overlapped, immediate_bytes_transferred| {
-                    let wsabuf = WSABUF {
-                        len: buffer.len() as u32,
-                        buf: PSTR::from_raw(buffer.as_mut_ptr()),
-                    };
-
-                    let wsabufs = [wsabuf];
-                    let mut flags: u32 = 0;
-
-                    winsock::to_io_result(WSARecv(
-                        **self.socket,
-                        &wsabufs,
-                        Some(immediate_bytes_transferred as *mut u32),
-                        &mut flags as *mut u32,
-                        Some(overlapped),
-                        None,
-                    ))
-                },
-            )
-        }
+        socket_receive(&self.socket, buffer)
     }
 
     /// Sends a buffer of data to the peer.
@@ -59,28 +38,7 @@ impl TcpConnection {
     /// You may call this multiple times concurrently. The buffers will be sent in the order they
     /// are submitted.
     pub fn send(&mut self, buffer: PinnedBuffer) -> OperationResultFuture {
-        // SAFETY: We are required to pass the OVERLAPPED pointer to the completion routine. We do.
-        unsafe {
-            current_async_agent::with_io(|io| io.new_operation(buffer)).begin(
-                |buffer, overlapped, immediate_bytes_transferred| {
-                    let wsabuf = WSABUF {
-                        len: buffer.len() as u32,
-                        buf: PSTR::from_raw(buffer.as_mut_ptr()),
-                    };
-
-                    let wsabufs = [wsabuf];
-
-                    winsock::to_io_result(WSASend(
-                        **self.socket,
-                        &wsabufs,
-                        Some(immediate_bytes_transferred as *mut u32),
-                        0,
-                        Some(overlapped),
-                        None,
-                    ))
-                },
-            )
-        }
+        socket_send(&self.socket, buffer)
     }
 
     /// Performs a graceful shutdown of the connection, allowing time for all pending data transfers
@@ -89,15 +47,6 @@ impl TcpConnection {
     ///
     /// An error result indicates that a graceful shutdown was not possible.
     pub async fn shutdown(&mut self) -> io::Result<()> {
-        // How this works is that we:
-        // 1) Tell the OS that we will not send any more data. This sets the wheels in motion.
-        // 2) Try to read more data from the peer and expect to see a 0-byte result (EOF).
-        //    Actually getting some data here is an error - this indicates that the caller did not
-        //    fully process incoming data before calling shutdown. Potentially valid for some types
-        //    of connections (e.g. an infinite stream of values) but not typical for web APIs.
-        // 3) Done! Once we get the EOF, we can be sure that the peer has received all of our data
-        //    and our FIN has been acknowledged, so no more activity can occur on the wire
-
         let socket_clone = Arc::clone(&self.socket);
         current_runtime::with(|runtime| {
             runtime.spawn_sync(SynchronousTaskType::Syscall, move || {
@@ -121,3 +70,165 @@ impl TcpConnection {
 impl !Send for TcpConnection {}
 #[negative_impl]
 impl !Sync for TcpConnection {}
+
+fn socket_receive(socket: &SOCKET, buffer: PinnedBuffer) -> OperationResultFuture {
+    // SAFETY: We are required to pass the OVERLAPPED pointer to the completion routine. We do.
+    unsafe {
+        current_async_agent::with_io(|io| io.new_operation(buffer)).begin(
+            |buffer, overlapped, immediate_bytes_transferred| {
+                let wsabuf = WSABUF {
+                    len: buffer.len() as u32,
+                    buf: PSTR::from_raw(buffer.as_mut_ptr()),
+                };
+
+                let wsabufs = [wsabuf];
+                let mut flags: u32 = 0;
+
+                winsock::to_io_result(WSARecv(
+                    *socket,
+                    &wsabufs,
+                    Some(immediate_bytes_transferred as *mut u32),
+                    &mut flags as *mut u32,
+                    Some(overlapped),
+                    None,
+                ))
+            },
+        )
+    }
+}
+
+fn socket_send(socket: &SOCKET, buffer: PinnedBuffer) -> OperationResultFuture {
+    // SAFETY: We are required to pass the OVERLAPPED pointer to the completion routine. We do.
+    unsafe {
+        current_async_agent::with_io(|io| io.new_operation(buffer)).begin(
+            |buffer, overlapped, immediate_bytes_transferred| {
+                let wsabuf = WSABUF {
+                    len: buffer.len() as u32,
+                    buf: PSTR::from_raw(buffer.as_mut_ptr()),
+                };
+
+                let wsabufs = [wsabuf];
+
+                winsock::to_io_result(WSASend(
+                    *socket,
+                    &wsabufs,
+                    Some(immediate_bytes_transferred as *mut u32),
+                    0,
+                    Some(overlapped),
+                    None,
+                ))
+            },
+        )
+    }
+}
+
+#[derive(Debug)]
+#[pin_project]
+pub struct ShutdownFuture {
+    socket: Arc<OwnedHandle<SOCKET>>,
+
+    #[pin]
+    state: ShutdownState,
+}
+
+#[derive(Debug)]
+#[pin_project(project = ShutdownStateProj)]
+enum ShutdownState {
+    // 1) Tell the OS that we will not send any more data. This sets the wheels in motion.
+    Disconnecting(#[pin] RemoteJoinHandle<io::Result<()>>),
+
+    // 2) Try to read more data from the peer and expect to see a 0-byte result (EOF).
+    //    Actually getting some data here is an error - this indicates that the caller did not
+    //    fully process incoming data before calling shutdown. Potentially valid for some types
+    //    of connections (e.g. an infinite stream of values) but not typical for web APIs.
+    WaitingForEndOfStream(#[pin] OperationResultFuture),
+
+    // 3) Done! Once we get the EOF, we can be sure that the peer has received all of our data
+    //    and our FIN has been acknowledged, so no more activity can occur on the wire
+    //    Should not be polled again in this state - we have returned Ready already.
+    Completed,
+}
+
+impl ShutdownFuture {
+    fn new(socket: Arc<OwnedHandle<SOCKET>>) -> Self {
+        let disconnecting = current_runtime::with({
+            let socket = Arc::clone(&socket);
+
+            |runtime| {
+                // We send this as high priority as it is a "release resources" operation that may
+                // have efficiency benefits to do as soon as possible.
+                runtime.spawn_sync(SynchronousTaskType::HighPrioritySyscall, move || {
+                    // SAFETY: Socket liveness is ensured by our shared ownership of the socket handle.
+                    winsock::to_io_result(unsafe { WSASendDisconnect(**socket, None) })
+                })
+            }
+        });
+
+        Self {
+            socket,
+            state: ShutdownState::Disconnecting(disconnecting),
+        }
+    }
+}
+
+impl Future for ShutdownFuture {
+    type Output = io::Result<()>;
+
+    fn poll(
+        self: std::pin::Pin<&mut Self>,
+        cx: &mut std::task::Context<'_>,
+    ) -> std::task::Poll<Self::Output> {
+        let mut this = self.project();
+
+        // We loop for fallthrough between some of the states.
+        loop {
+            match this.state.as_mut().project() {
+                ShutdownStateProj::Disconnecting(join_handle) => {
+                    match join_handle.poll(cx) {
+                        Poll::Ready(Ok(())) => {}
+                        Poll::Ready(Err(e)) => {
+                            *this.state = ShutdownState::Completed;
+                            return Poll::Ready(Err(e));
+                        }
+                        Poll::Pending => {
+                            *this.state = ShutdownState::Completed;
+                            return Poll::Pending;
+                        }
+                    };
+
+                    let receive_future = socket_receive(this.socket, PinnedBuffer::from_pool());
+
+                    *this.state = ShutdownState::WaitingForEndOfStream(receive_future);
+                    // We fall through to the next state here.
+                }
+                ShutdownStateProj::WaitingForEndOfStream(receive_future) => {
+                    match receive_future.poll(cx) {
+                        Poll::Ready(Ok(buffer)) => {
+                            if !buffer.is_empty() {
+                                *this.state = ShutdownState::Completed;
+                                return Poll::Ready(Err(io::Error::LogicError("socket received data when shutting down - this may be an error depending on the communication protocol in use".to_string())));
+                            }
+
+                            *this.state = ShutdownState::Completed;
+                            return Poll::Ready(Ok(()));
+                        }
+                        Poll::Ready(Err(e)) => {
+                            *this.state = ShutdownState::Completed;
+                            return Poll::Ready(Err(e.into_inner()));
+                        }
+                        Poll::Pending => {
+                            return Poll::Pending;
+                        }
+                    };
+                }
+                // The Future API allows us to panic in this situation.
+                ShutdownStateProj::Completed => panic!("ShutdownFuture polled after completion"),
+            }
+        }
+    }
+}
+
+#[negative_impl]
+impl !Send for ShutdownFuture {}
+#[negative_impl]
+impl !Sync for ShutdownFuture {}
